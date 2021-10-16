be2fb8024bbe847c4ca786715cdcdb79cafc5bb0
ggxc3 <57632051+ggxc3@users.noreply.github.com>
2021-10-16 16:17:41 +0200

    Pridanie windows forms, zaistene spustenie konzoly

--- ExcelSuradniceScript/Program.cs
+++ ExcelSuradniceScript/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace ExcelSuradniceScript
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AllocConsole();
+
+            var dialog = new OpenFileDialog();
+            dialog.ShowDialog();
         }
+        
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
     }
 }
