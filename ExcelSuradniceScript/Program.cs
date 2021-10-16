2dff684950676a046fdc4a6317cb96e82e276ac7
ggxc3 <57632051+ggxc3@users.noreply.github.com>
2021-10-16 11:36:01 +0200

    Initial commit

---
+++ ExcelSuradniceScript/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExcelSuradniceScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
